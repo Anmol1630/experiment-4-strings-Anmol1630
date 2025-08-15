33372132c23f022783fc425b5fe34d350e8da447
Anmol Prashar <114831229+Anmol1630@users.noreply.github.com>
2025-08-15 16:20:31 +0530

    Delete rotatestring.java

--- rotatestring.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean rotateString(String s, String goal) {
-        if (s.length() != goal.length()) {
-            return false;
-        }
-        String comb = s + s;
-        return comb.contains(goal);
-    }
-}
